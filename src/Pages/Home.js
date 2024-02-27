4a6355680657544e0cd76c708e3415117100e73f
yashrajoriya369 <154687772+yashrajoriya369@users.noreply.github.com>
2024-02-27 19:32:29 +0530

    update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -71,7 +71,7 @@ const Home = () => {
 
       <section className="client-transformation">
         <div className="container row ">
-          <div className="card col-sm-4 me-5">
+          <div className="card col-sm-4">
             <div className="card-header">
               <img
                 src="https://s.aolcdn.com/images/dims?client=fh7w6q744eiognjk&signature=d59d0cf6af1d779a3dca451e0ba259c33bbc6115&image_uri=https%3A%2F%2Fs.aolcdn.com%2Fos%2Fab%2F_cms%2F2019%2F08%2F30142658%2F2020-jeep-wrangler-16.jpg&thumbnail=750%2C422&quality=80"
@@ -97,7 +97,7 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div className="card col-sm-4 me-5">
+          <div className="card col-sm-4 mx-2">
             <div className="card-header">
               <img
                 src="https://images.cruisecritic.com/image/18740535/10-best-cruise-destinations-for-hot-air-balloon-rides_600x400_21.jpg"
@@ -126,7 +126,7 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div className="card col-sm-4 me-5">
+          <div className="card col-sm-4">
             <div className="card-header">
               <img
                 src="https://dynaimage.cdn.cnn.com/cnn/q_auto,w_412,c_fill,g_auto,h_232,ar_16:9/http%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F200305114843-01-edge-hudson-yards-observation-deck.jpg"
